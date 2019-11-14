375e888d7e37781ce68d26af12b7a2e1d47104dd
Rikesh <sr1k3sh@gmail.com>
2019-11-14 13:37:09 +0545

    inital commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Test extends Component {
+    render() {
+        return (
+           <React.Fragment>
+               <img src={this.props.src}></img>
+           </React.Fragment>
+        )
+    }
+}
