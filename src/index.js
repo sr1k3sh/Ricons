71aeadbfb6ec7cdff9e39bbc4b0e1eb759817b73
Rikesh <sr1k3sh@gmail.com>
2019-11-14 15:27:07 +0545

    second update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class Test extends Component {
+export default class Ricon extends Component {
     render() {
         return (
            <React.Fragment>
